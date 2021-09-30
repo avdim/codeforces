e09613b6547775c791161f0f20856ce0efd249bf
avdim <avdim@mail.ru>
2021-09-30 18:36:36 +0300

    solveSudoku

--- kt/src/SodukuSolverTest.kt
+++ kt/src/SodukuSolverTest.kt
@@ -1,42 +1,46 @@
 import org.junit.Test
-import kotlin.random.Random
-import kotlin.test.assertEquals
 
 class SodukuSolverTest {
-    @Test
-    fun solveSoduku2x2() {
-        // let input = [
-        //     [".", ".", "1", "."],
-        //     [".", ".", ".", "."],
-        //     [".", ".", "3", "."],
-        //     [".", ".", "2", "."],
-        // ];
+  @Test
+  fun solveSoduku2x2() {
+    // let input = [
+    //     [".", ".", "1", "."],
+    //     [".", ".", ".", "."],
+    //     [".", ".", "3", "."],
+    //     [".", ".", "2", "."],
+    // ];
 
-        // let input = [
-        //     ["4", ".", "1", "."],
-        //     [".", "2", ".", "."],
-        //     [".", ".", "3", "."],
-        //     [".", ".", "2", "."],
-        // ];
-    }
+    // let input = [
+    //     ["4", ".", "1", "."],
+    //     [".", "2", ".", "."],
+    //     [".", ".", "3", "."],
+    //     [".", ".", "2", "."],
+    // ];
+  }
 
-    @Test
-    fun solveSudoku3x3() {
-        val input = listOf(
-            listOf("5", "3", ".", ".", "7", ".", ".", ".", "."),
-            listOf("6", ".", ".", "1", "9", "5", ".", ".", "."),
-            listOf(".", "9", "8", ".", ".", ".", ".", "6", "."),
-            listOf("8", ".", ".", ".", "6", ".", ".", ".", "3"),
-            listOf("4", ".", ".", "8", ".", "3", ".", ".", "1"),
-            listOf("7", ".", ".", ".", "2", ".", ".", ".", "6"),
-            listOf(".", "6", ".", ".", ".", ".", "2", "8", "."),
-            listOf(".", ".", ".", "4", "1", "9", ".", ".", "5"),
-            listOf(".", ".", ".", ".", "8", ".", ".", "7", "9"),
-        );
-    }
+  @Test
+  fun solveSudoku3x3() {
+    val input = listOf(
+      listOf("5", "3", ".", ".", "7", ".", ".", ".", "."),
+      listOf("6", ".", ".", "1", "9", "5", ".", ".", "."),
+      listOf(".", "9", "8", ".", ".", ".", ".", "6", "."),
+      listOf("8", ".", ".", ".", "6", ".", ".", ".", "3"),
+      listOf("4", ".", ".", "8", ".", "3", ".", ".", "1"),
+      listOf("7", ".", ".", ".", "2", ".", ".", ".", "6"),
+      listOf(".", "6", ".", ".", ".", ".", "2", "8", "."),
+      listOf(".", ".", ".", "4", "1", "9", ".", ".", "5"),
+      listOf(".", ".", ".", ".", "8", ".", ".", "7", "9"),
+    );
+    println(solveSudokuString(input))
+  }
 
 }
 
-fun solveSudoku(matrix:List<List<String>>) {
+fun solveSudokuString(input: List<List<String>>) {
+  val matrix: List<List<Int?>> = input.map { it.map { it.toIntOrNull() } }
+  solveSudoku(matrix)
+}
+
+fun solveSudoku(matrix:List<List<Int?>>) {
 
 }
