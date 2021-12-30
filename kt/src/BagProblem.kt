2b216ab8f4f4b652da87bddbd14d5a0b6802fb0c
avdim <avdim@mail.ru>
2021-12-30 23:07:43 +0300

    fillBag

--- kt/src/BagProblem.kt
+++ kt/src/BagProblem.kt
@@ -4,7 +4,7 @@ import kotlin.test.assertEquals
 
 class BagProblem() {
 
-  val randomItems = List(40) {
+  val randomItems = List(20) {
     Item(Random.nextDouble(), Random.nextDouble())
   }
 
@@ -26,29 +26,26 @@ class BagProblem() {
   fun testBinaryData() {
     val items = List(10) { Item(1.0, 1.0) } + List(10) { Item(1.0, 2.0) }
     val result = fillBag(items, 5.0)
-    result.forEach {
-      println(it)
-    }
   }
 
   data class Item(val size: Double, val cost: Double)
 
   private val List<Item>.cost get() = sumOf { it.cost }
 
-  private fun fillBag(items: List<Item>, bagSize: Double): List<Item> {
+  private fun fillBag(
+    items: List<Item>,
+    bagSize: Double
+  ): List<Item> {
     if (items.isEmpty()) {
       return emptyList()
     }
     if (bagSize <= 0) {
       return emptyList()
     }
-
     val withoutFirst = items.drop(1)
     val first = items[0]
-
     val fillWithFirst = listOf(first) + fillBag(withoutFirst, bagSize - first.size)
     val fillWithoutFirst = fillBag(withoutFirst, bagSize)
-
     return if (fillWithFirst.cost > fillWithoutFirst.cost) {
       fillWithFirst
     } else {
