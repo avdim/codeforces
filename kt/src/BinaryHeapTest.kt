12fe71b2b40674e8a646c6eeffeafc40800f0d73
avdim <avdim@mail.ru>
2021-12-31 10:05:20 +0300

    Heap removeMin

--- kt/src/BinaryHeapTest.kt
+++ kt/src/BinaryHeapTest.kt
@@ -10,6 +10,7 @@ class BinaryHeapTest {
     val a: MutableList<T> = mutableListOf()
     fun insert(x: T) {
       a.add(x)
+      //sift up
       var i = a.lastIndex
       while (i.parentIndex >= 0 && a[i.parentIndex] < a[i]) {
         a.swap(i, i.parentIndex)
@@ -21,8 +22,35 @@ class BinaryHeapTest {
       return a.firstOrNull()
     }
 
-    fun remove_min() {
+    fun removeMin(): T? {
+      if (a.size == 0) {
+        return null
+      }
+      a.swap(0, a.lastIndex)
+      val result = a.removeLast()
+      //sift down
 
+      fun Int.leftChild() = a[leftChildIndex]
+      fun Int.rightChild() = a[rightChildIndex]
+      var i = 0
+      while (true) {
+        val indexOfMinChild: Int = if (i.rightChildIndex < a.size) {
+          if (i.leftChild() < i.rightChild()) i.leftChildIndex else i.rightChildIndex
+        } else {
+          if (i.leftChildIndex < a.size) {
+            i.leftChildIndex
+          } else {
+            break
+          }
+        }
+        if (a[i] > a[indexOfMinChild]) {
+          a.swap(i, indexOfMinChild)
+          i = indexOfMinChild
+        } else {
+          break
+        }
+      }
+      return result
     }
   }
 
